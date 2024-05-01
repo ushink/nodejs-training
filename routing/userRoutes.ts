86cbba40c52d42c997ecd82c1956e9953594e802
Ulyana <ushinkevich@mail.ru>
2024-05-01 17:33:56 +0400

    post user OK

--- routing/userRoutes.ts
+++ routing/userRoutes.ts
@@ -2,6 +2,7 @@ import { Request, Response, Router } from "express";
 import crypto from "crypto";
 import { users } from "../mock";
 import { paramsType, userType } from "../models";
+import { User } from "../models/user.model";
 
 const router = Router();
 router.get("/", (req: Request, res: Response) => {
@@ -13,25 +14,13 @@ router.get("/users", (req: Request, res: Response) => {
 });
 
 router.post("/users", (req: Request<{}, {}, userType>, res: Response) => {
-  // обработка post если указаны не те данные или их нет
-  const { id, name, age, isAdmin, experience } = req.body;
-
-  if (id) {
-    return res.status(422).json({ message: "Нужно убрать id из объекта" });
-  }
-
-  if (!isAdmin) {
-    res.status(422).json({ message: "Поле isAdmin обязательное" }); //TODO: Спросить почему если в поле isAdmin false выпадает ошибка
-    return;
-  }
-
-  //генерируем уникальный id
-  const uniqId = crypto.randomUUID();
-
-  //добавляем нового пользователя в массив
-  users.push({ ...req.body, id: uniqId });
-
-  res.status(200).json({ message: "Пользователь успешно добавлен" });
+  User.create(req.body)
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((error) => {
+      res.status(500).json({ message: error.message });
+    });
 });
 
 router.get("/users/:id", (req: Request<paramsType>, res: Response) => {
