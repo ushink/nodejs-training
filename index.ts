6cc73cdcd52c7bd2de13b40a7fc236a0f441e6a8
Ulyana <ushinkevich@mail.ru>
2024-05-01 13:22:42 +0400

    get users

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from "express";
+import { users } from "./mock";
 
 // const express = require('express')
 
@@ -10,6 +11,10 @@ app.get("/", (req: Request, res: Response) => {
   res.send("hello world");
 });
 
+app.get("/users", (req: Request, res: Response) => {
+    res.send(users);
+  });
+
 app.listen(port, () => {
-  console.log("server is running");
+  console.log(`server is running on port ${port}`);
 });
---
+++ mock/index.ts
@@ -0,0 +1,31 @@
+type userType = {
+  id: string;
+  name: string;
+  age: number;
+  isAdmin: boolean;
+  experience: number;
+};
+
+export const users: userType[] = [
+  {
+    id: "1",
+    name: "Misha",
+    age: 19,
+    isAdmin: false,
+    experience: 0,
+  },
+  {
+    id: "2",
+    name: "Tanya",
+    age: 28,
+    isAdmin: false,
+    experience: 3,
+  },
+  {
+    id: "3",
+    name: "Alex",
+    age: 41,
+    isAdmin: false,
+    experience: 3,
+  },
+];
