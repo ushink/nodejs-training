8caf443256d338df66e37b3f44fcb1ff4921edb9
Ulyana <ushinkevich@mail.ru>
2024-05-01 14:07:13 +0400

    post user

--- index.ts
+++ index.ts
@@ -1,9 +1,14 @@
 import express, { Request, Response } from "express";
 import { users } from "./mock";
+import crypto from "crypto";
 
+// import crypto = require("crypto");
 // const express = require('express')
 
 const app = express();
+
+// для обработки post запросов
+app.use(express.json());
 
 const port = 3001;
 
@@ -12,9 +17,18 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.get("/users", (req: Request, res: Response) => {
-    res.send(users);
-  });
+  res.send(users);
+});
 
+app.post("/users", (req: Request, res: Response) => {
+  //генерируем уникальный id
+  const id = crypto.randomUUID();
+  
+  //добавляем нового пользователя в массив
+  users.push({ ...req.body, id });
+
+  res.status(200).json({ message: "Пользователь успешно добавлен" });
+});
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
 });
