52f85a5353512d7363deb50b01b672c3d69d084c
Ulyana <ushinkevich@mail.ru>
2024-05-01 18:22:00 +0400

    add controllers

--- routing/userRoutes.ts
+++ routing/userRoutes.ts
@@ -1,71 +1,22 @@
-import { Request, Response, Router } from "express";
-import { paramsType, userType } from "../models";
-import { User } from "../models/user.model";
+import { Router } from "express";
+import {
+  addUser,
+  deleteUser,
+  getUserById,
+  getUsers,
+  updateUser,
+} from "../controllers/user.controllers";
 
 const router = Router();
-router.get("/", (req: Request, res: Response) => {
-  res.send("hello world");
-});
 
-router.get("/users", (req: Request, res: Response) => {
-  User.find()
-    .then((data) => {
-      res.status(200).json(data);
-    })
-    .catch((error) => {
-      res.status(500).json({ message: error.message });
-    });
-});
+router.get("/users", getUsers);
 
-router.post("/users", (req: Request<{}, {}, userType>, res: Response) => {
-  User.create(req.body)
-    .then((data) => {
-      res.status(200).json(data);
-    })
-    .catch((error) => {
-      res.status(500).json({ message: error.message });
-    });
-});
+router.post("/users", addUser);
 
-router.get("/users/:id", (req: Request<paramsType>, res: Response) => {
-  //получаем id пользователя из параметров запроса
-  const { id } = req.params;
+router.get("/users/:id", getUserById);
 
-  User.findById(id)
-    .then((data) => {
-      res.status(200).json(data);
-    })
-    .catch((error) => {
-      res.status(500).json({ message: error.message });
-    });
-});
+router.put("/users/:id", updateUser);
 
-router.put(
-  "/users/:id",
-  (req: Request<paramsType, {}, userType>, res: Response) => {
-    const { id: idParams } = req.params;
-
-    User.findByIdAndUpdate(idParams, req.body)
-      .then((user) => User.findById(idParams))
-      .then((data) => {
-        res.status(200).json(data);
-      })
-      .catch((err) => {
-        res.status(500).json({ message: err.message });
-      });
-  }
-);
-
-router.delete("/users/:id", (req: Request<paramsType>, res: Response) => {
-  const { id } = req.params;
-
-  User.findByIdAndDelete(id)
-    .then(() => {
-      res.status(200).json({ message: "Пользователь успешно удален" });
-    })
-    .catch((err) => {
-      res.status(500).json({ message: err.message });
-    });
-});
+router.delete("/users/:id", deleteUser);
 
 export default router;
